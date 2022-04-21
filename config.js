0fb743ee4b6870f13f920c14929a6364775e01f9
LeNguyenMinh <90587555+LeNguyenMinh@users.noreply.github.com>
2022-04-21 23:55:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-const nameGirl = 'Em';
-const giftUrl = 'http://nodemy.vn';
-const eventName = 'Chúc Mừng 20-10';
-const titleCard = 'Tặng người ấy';
-const contentCard = 'Chúc honey của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+const nameGirl = 'Khánh Linh';
+const giftUrl = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+const eventName = 'Chúc Mừng Sinh Nhật ';
+const titleCard = 'KhanhLinh sinh nhat dui de';
+const contentCard = 'Mặc dù là hơi muộn nhưng mà sinh nhật vui vẻ nha, qua tuổi mới đừng làm lạnh gơ nữa mà cười nhiều lên nhe :))';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
